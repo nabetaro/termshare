a208c258fafb9fdf60880456bdbe11c772aaaa5f
nabetaro <kurasawa.nozomu@keiken.co.jp>
2012-05-30 16:19:56 +0900

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Termshare::Application.config.secret_token = '2433c5f7f0c2584d990fe52c06ba363112590662995fb08c16e06eb19df6ca346093c5acab6fb134e4ec39360963320bd1fb29e058e627da660cf2a1f2b2d531'
